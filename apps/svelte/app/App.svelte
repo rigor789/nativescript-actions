1a1cde86fc60a8eae6c5478f3df9eabb00d163f1
rigor789 <rigor789@gmail.com>
2020-05-17 16:17:16 +0200

    feat: initial repo with demo apps

---
+++ apps/svelte/app/App.svelte
@@ -0,0 +1,24 @@
+<page>
+    <actionBar title="Svelte Native App" />
+    <gridLayout>
+        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
+            <formattedString>
+                <span class="fas" text="&#xf135;" />
+                <span text=" {message}" />
+            </formattedString>
+        </label>
+    </gridLayout>
+</page>
+
+<script>
+    let message = "Blank Svelte Native App"
+</script>
+
+<style>
+    .info .fas {
+        color: #3A53FF;
+    }
+    .info {
+        font-size: 20;
+    }
+</style>
